2c97003461f2ccb130b3b473bb11b572c1f695c4
dizid <marc@dizid.com>
2019-10-18 17:06:11 +0200

    Added Service worker

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,6 +13,22 @@ module.exports = {
         path: 'blog/*.md',
         typeName: 'Post'
       }
+    },
+    ,
+      {
+        use: 'gridsome-plugin-pwa',
+        options: {
+            title: 'Plants & Programs',
+            startUrl: '/',
+            display: 'standalone',
+            statusBarStyle: 'default',
+            manifestPath: 'manifest.json',
+            serviceWorkerPath: 'service-worker.js',
+            shortName: 'P&P',
+            themeColor: '#666600',
+            backgroundColor: '#ffffff',
+            icon: './static/favicon.png'
+        }
     }
   ]
 }
